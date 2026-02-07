26ded2edfb12ddf73932e8b429f9e925e07cc233
chatwinho-lab <chatwinho@g.ucla.edu>
2026-02-06 18:37:58 -0800

    chore: motor testing scripts

---
+++ _test/motor_test_chat.py
@@ -0,0 +1,68 @@
+import argparse
+import time
+import busio
+from board import SCL, SDA
+from adafruit_pca9685 import PCA9685
+import numpy as np
+
+parser = argparse.ArgumentParser(description="Motor testing script!")
+parser.add_argument("-e", "--esc", type=int, default=1, help="ESC channel (default: 1)")
+parser.add_argument(
+    "-s", "--servo", type=int, default=0, help="Servo channel (default: 0)"
+)
+parser.add_argument(
+    "-i",
+    "--initialize",
+    type=int,
+    default="0",
+    help="Is the motor running for the first time since being switched on? (default: 0)",
+)
+# parser.add_argument(' -- extra_option', type=str, default = "", help='Message to explain the option (default: empty string)') #Template for additional arguments
+
+args = parser.parse_args()
+ESC_CHANNEL = args.esc
+SERVO_CHANNEL = args.servo
+# If the motor is running for the first time since being switched on, the ESC needs to be "armed" (meaning that we need to send a neutral signal for 2 seconds)
+arm_ESC = args.initialize
+
+# extra_argument = args.extra_option
+
+# === Constants ===
+
+FREQ = 60  # Standard ESC expects 50-60Hz
+NEUTRAL = 1500
+MAX = 1900
+MIN = 1200
+
+# Setup I2C bus and PCA9685
+i2c = busio.I2C(SCL, SDA)
+pca = PCA9685(i2c)
+pca.frequency = FREQ
+
+MAXD = 65535
+
+# ARM THE THING
+print("arming...")
+pca.channels[0].duty_cycle = 5897
+time.sleep(1)
+pca.channels[1].duty_cycle = 5897
+time.sleep(2)
+
+
+# channels[0] is the servo
+# channels[1] is the motor
+
+# servo: 6-12%
+# motor:
+
+
+print("going...")
+pca.channels[0].duty_cycle = int(MAXD * 0.12)
+time.sleep(1)
+pca.channels[1].duty_cycle = int(MAXD * 0.1)
+time.sleep(1)
+
+# SHUT IT OFF
+pca.channels[0].duty_cycle = 0
+pca.channels[1].duty_cycle = 0
+pca.deinit()
